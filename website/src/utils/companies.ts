50132d90147463f5032236e4223f82d122ef4d87
Kostek <ksytnik73kwer@gmail.com>
2024-03-17 16:51:54 +0100

    Add new fontFamily in tailwind config. Add google font in globals.css. Import React to fix small issue. Create an array: linksCompanies for infiniteSlider component

---
+++ website/src/utils/companies.ts
@@ -0,0 +1,23 @@
+import FacebookLogo from "../../public/facebook.png";
+import GoogleLogo from "../../public/google.png";
+import AmazonLogo from "../../public/amazon.png";
+import SlackLogo from "../../public/slack.png";
+import GitKrakenLogo from "../../public/gitkraken.png";
+import MicrosoftLogo from "../../public/microsoft.png";
+import DropBoxLogo from "../../public/dropbox.png";
+import EbayLogo from "../../public/ebay.png";
+import IntelLogo from "../../public/intel.png";
+import NetflixLogo from "../../public/netflix.png";
+
+export const linksCompanies = [
+    { image: FacebookLogo, brand: "Facebook", to: "https://www.facebook.com/" },
+    { image: GoogleLogo, brand: "Google", to: "https://about.google/" },
+    { image: AmazonLogo, brand: "Amazon", to: "https://www.amazon.com/" },
+    { image: SlackLogo, brand: "Slack", to: "https://slack.com/" },
+    { image: GitKrakenLogo, brand: "GitKraken", to: "https://www.gitkraken.com/" },
+    { image: MicrosoftLogo, brand: "Microsoft", to: "https://www.microsoft.com/en-us/" },
+    { image: DropBoxLogo, brand: "Dropbox", to: "https://www.dropbox.com/" },
+    { image: EbayLogo, brand: "Ebay", to: "https://www.ebay.com/" },
+    { image: IntelLogo, brand: "Intel", to: "https://www.intel.com/" },
+    { image: NetflixLogo, brand: "Netflix", to: "https://www.netflix.com/" },
+];

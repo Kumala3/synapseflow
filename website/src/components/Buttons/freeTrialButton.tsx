4673f4be0519106beefa6f94a6c8b984c2358c0b
Kostek <ksytnik73kwer@gmail.com>
2024-03-20 22:15:45 +0100

    Add FreeTrialButton and PricingButton components

---
+++ website/src/components/Buttons/freeTrialButton.tsx
@@ -0,0 +1,7 @@
+export function FreeTrialButton() {
+    return (
+        <button className="bg-[#3913df] w-1/2 py-4 rounded-md text-white lg:w-1/6 lg:hover:opacity-85 transition-all duration-300 ease-in-out">
+            Try For Free
+        </button>
+    );
+}
---
+++ website/src/components/Buttons/pricingButton.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import ArrowButton from "../../../public/arrow-button.svg";
+
+
+export function PricingButton() {
+    return (
+        <button className="flex items-center justify-center gap-x-2 text-[#3612d5] w-1/2 rounded-md font-semibold">
+            View Pricing
+            <Link href="/pricing">
+                <Image
+                    src={ArrowButton}
+                    alt="arrow-button"
+                    width={40}
+                    className="hover:scale-110 hover:opacity-70 transition-all duration-300 ease-in-out"
+                />
+            </Link>
+        </button>
+    );
+}

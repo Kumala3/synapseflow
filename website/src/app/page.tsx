2d672af4d04a0540f65719719d002480d4ede6e7
Kostek <ksytnik73kwer@gmail.com>
2024-03-22 21:09:42 +0100

    Add Features component to Home page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Features />
+        </div>
+    );
 }

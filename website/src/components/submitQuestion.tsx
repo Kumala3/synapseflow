de500ea4d39a0490811372c597b82ebc102e61ed
Kostek <ksytnik73kwer@gmail.com>
2024-03-23 17:52:01 +0100

    Add textarea instead of simple input component and character count functionality

--- website/src/components/submitQuestion.tsx
+++ website/src/components/submitQuestion.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { Input } from "@nextui-org/react";
+import { Textarea } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 
 interface FormState {
@@ -31,17 +32,25 @@ export default function SubmitQuestion() {
 
         // Clear errors for that field when the user starts typing
         if (errors[name]) {
-            setErrors(prevErrors => ({ ...prevErrors, [name]: undefined}));
+            setErrors(prevErrors => ({ ...prevErrors, [name]: undefined }));
         }
+    };
+
+    const handleText = () => {
+        return (
+            <p className="flex flex-row gap-1 text-[12px] text-[#1e37d9]">
+                You have typed <p className="font-semibold">{form.question.length} / 800</p> characters
+            </p>
+        );
     };
 
     const handleClear = (name: string) => {
         // Clear textarea when button clear was clicked
         setForm(prevForm => ({ ...prevForm, [name]: "" }));
-        
+
         // Clear errors for that field when user has clicked on clear button
         if (errors[name]) {
-            setErrors(prevErrors => ({ ...prevErrors, [name]: undefined}));
+            setErrors(prevErrors => ({ ...prevErrors, [name]: undefined }));
         }
     };
 
@@ -59,6 +68,8 @@ export default function SubmitQuestion() {
         // Validate if question is empty
         if (!form.question) {
             newErrors.question = "Please write a question before submitting.";
+        } else if (form.question.length > 800) {
+            newErrors.question = "Question should not exceed 800 characters.";
         }
 
         // Update error state
@@ -68,13 +79,13 @@ export default function SubmitQuestion() {
         if (Object.keys(newErrors).length === 0) {
             // You can submit the form
             console.log("Submitting form:", form);
-            console.log("Submit Question works fine!");
+            console.log("Submit Question component works perfectly!");
             // Here you would normally make an API call
         }
     };
 
     return (
-        <div>
+        <div className="">
             <h1 className="font-bold text-[28px]">
                 Ask your personal question:
             </h1>
@@ -95,10 +106,10 @@ export default function SubmitQuestion() {
                 variant="bordered"
             />
 
-            <Input
+            <Textarea
                 name="question"
-                type="text"
-                placeholder="Your question"
+                label="Question"
+                placeholder="Write your question here..."
                 errorMessage={errors.question}
                 isInvalid={!!errors.question}
                 value={form.question}
@@ -107,8 +118,10 @@ export default function SubmitQuestion() {
                 size="lg"
                 isRequired={true}
                 onClear={() => handleClear("question")}
+                description={handleText()}
                 className="pt-5"
                 variant="bordered"
+                maxRows={4}
             />
 
             <div className="flex pt-5 justify-center">

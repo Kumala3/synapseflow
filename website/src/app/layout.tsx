ce7632fe51c1df8c9d15c5a79e61d002d80e81a2
Kostek <ksytnik73kwer@gmail.com>
2024-03-24 18:12:26 +0100

    Add Navbar component to layout and remove Navbar component from page

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
         <html lang="en" className="light">
             <body className={inter.className}>
 				<Providers>
+                    <Navbar />
 					{children}
 				</Providers>
             </body>
--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar";
 import Introduction from "@/components/Introduction";
 import Features from "@/components/Features";
 import Faq from "@/components/Faq";
@@ -7,7 +6,6 @@ import Plans from "@/components/Plans";
 export default function Home() {
     return (
         <div>
-            <Navbar />
             <Introduction />
             <Features />
             <Faq />

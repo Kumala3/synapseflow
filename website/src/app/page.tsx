866cbf5ab40b5274271aba3e5f342b1551368464
Kostek <ksytnik73kwer@gmail.com>
2024-03-24 11:18:10 +0100

    Add Plans component and create Pricing page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Faq from "@/components/Faq";
+import Plans from "@/components/Plans";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Hero />
             <Features />
             <Faq />
+            <Plans />
         </div>
     );
 }
---
+++ website/src/app/pricing/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Pricing() {
+    return (
+        <div className="">
+            <h1>Hello from Pricing page!</h1>
+        </div>
+    );
+}

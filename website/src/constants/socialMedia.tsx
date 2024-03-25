32898ce117397b8eb410e0648705e7535ab967f2
Kostek <ksytnik73kwer@gmail.com>
2024-03-25 21:19:16 +0100

    Add social media constants

---
+++ website/src/constants/socialMedia.tsx
@@ -0,0 +1,11 @@
+import FacebookLogo from "../../public/facebooklogo.svg";
+import LinkedInLogo from "../../public/linkedinlogo.svg";
+import DiscordLogo from "../../public/discordlogo.svg";
+import XLogo from "../../public/xlogo.svg";
+
+export const socialMedia = [
+    { logo: FacebookLogo, url: "https://www.facebook.com"},
+    { logo: LinkedInLogo, url: "https://www.linkedin.com"},
+    { logo: XLogo, url: "https://twitter.com/"},
+    { logo: DiscordLogo, url: "https://www.discord.com"},
+];

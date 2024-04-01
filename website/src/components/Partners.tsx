85240cfa70870860264f8e0a6bb776838b16fa67
Kostek <ksytnik73kwer@gmail.com>
2024-04-01 22:34:07 +0200

    Refactor Partners component to use PartnersSkeleton for loading state

--- website/src/components/Partners.tsx
+++ website/src/components/Partners.tsx
@@ -1,12 +1,10 @@
 "use client";
 import InfiniteSlider from "./infiniteSlider";
 import { usePartnerCompanies } from "@/hooks/usePartnerCompanies";
-import LoadingData from "./LoadingData";
+import { PartnersSkeleton } from "./PartnersSkeleton";
 
 export default function Partners() {
-    const { partnerCompanies, isError, isLoading} = usePartnerCompanies();
-
-    if (isLoading) return <LoadingData />;
+    const { partnerCompanies, isError, isLoading } = usePartnerCompanies();
 
     if (isError) return <div>Failed to load data about partner_companies</div>;
 
@@ -14,13 +12,23 @@ export default function Partners() {
 
     return (
         <div className="flex flex-col">
-                <div className="-mt-52 lg:">
-                    <InfiniteSlider time={60} companies={partner_companies || []} />
+            {isLoading ? (
+                <div className="-mt-40">
+                    <PartnersSkeleton />
                 </div>
-
-                <div className="-mt-10">
-                    <InfiniteSlider direction={2} time={60} companies={partner_companies || []} />
+            ) : (
+                <div className="-mt-[220px] gap-0">
+                    <InfiniteSlider
+                        time={20}
+                        companies={partner_companies || []}
+                    />
+                    <InfiniteSlider
+                        direction={2}
+                        time={20}
+                        companies={partner_companies || []}
+                    />
                 </div>
-            </div>
+            )}
+        </div>
     );
 }
---
+++ website/src/components/PartnersSkeleton.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@nextui-org/react";
+
+export function PartnersSkeleton() {
+    return (
+        <div className="relative lg:max-w-[900px] max-w-[1000px] md:max-w-[650px] overflow-hidden h-[150px] mx-auto flex flex-nowrap [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+            <div className="flex flex-col gap-5">
+                <ul className="w-full flex gap-10 animate-infinite-scroll-left">
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                </ul>
+
+                <ul className="w-full flex gap-10 animate-infinite-scroll-left">
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                    <Skeleton className="h-[60px] w-[180px] rounded-md" disableAnimation={true}/>
+                </ul>
+            </div>
+        </div>
+    );
+}

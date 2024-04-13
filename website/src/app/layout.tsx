ebb86a298abf8be179b059b9544fc9a3c856eedb
Kostek <ksytnik73kwer@gmail.com>
2024-04-13 09:08:43 +0200

    Add @vercel/analytics to dependencies
    Add Analytics component to global layout of my application

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -6,6 +6,7 @@ import CustomNavbar from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
                     {children}
                     <Footer />
                     <SpeedInsights />
+                    <Analytics />
                 </Providers>
             </body>
         </html>

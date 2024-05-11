4e4de6180ebf356b3f06cd93fda4006f94a72392
Kostek <ksytnik73kwer@gmail.com>
2024-05-11 12:03:17 +0200

    Set up GitHub and Credentials providers in options.ts and initialize route.ts

---
+++ website/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,50 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "Username",
+                    type: "text",
+                    placeholder: "your-unique-username",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                },
+            },
+            async authorize(credentials, req) {
+                // Docs: https://next-auth.js.org/configuration/providers/credentials
+                // You need to provide your own logic here that takes the credentials
+                // submitted and returns either a object representing a user or value
+                // that is false/null if the credentials are invalid.
+                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+                // You can also use the `req` object to obtain additional parameters
+                // (i.e., the request IP address)
+                const res = await fetch("/your/endpoint", {
+                    method: "POST",
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" },
+                });
+                const user = await res.json();
+
+                // If no error and we have user data, return it
+                if (res.ok && user) {
+                    return user;
+                }
+                // Return null if user data could not be retrieved
+                return null;
+            },
+        }),
+    ],
+    // Read more about custom pages here: https://next-auth.js.org/configuration/options
+    // pages: {}
+};

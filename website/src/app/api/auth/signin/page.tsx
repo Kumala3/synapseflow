abbaa4078e88c063b5255e0da85f2ba99ace04f7
Kostek <ksytnik73kwer@gmail.com>
2024-05-12 22:07:17 +0200

    Create sign in page

---
+++ website/src/app/api/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import SignInForm from "@/components/SignIn/SignInForm";
+
+export default async function SignIn() {
+    return (
+        <main>
+            Sign in to your Account
+            <SignInForm />
+        </main>
+    );
+}

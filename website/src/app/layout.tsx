6c055d760749f562021bce8e97f06e5222b8549a
Kostek <ksytnik73kwer@gmail.com>
2024-05-21 17:59:03 +0200

    Delete Footer and Navbar components form the RootLayout

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-import CustomNavbar from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
@@ -24,9 +22,7 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
             <body className={`${inter.className} dark:bg-dark`} >
                 <Providers>
-                    <CustomNavbar />
                     {children}
-                    <Footer />
                     <SpeedInsights />
                     <Analytics />
                 </Providers>

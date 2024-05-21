3f3c1e6b950650e653e3515b3d474314d7f8c4ac
Kostek <ksytnik73kwer@gmail.com>
2024-05-21 17:59:15 +0200

    Add Footer and Navbar components to Home page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Introduction from "@/components/Introduction";
 import Features from "@/components/Features";
+import { Footer } from "@/components/Footer";
+import { GlobalNavbar } from "@/components/Navbar";
 
 export default function Home() {
     return (
-        <main className=" xl:mx-auto xl:container">
+        <main className="xl:mx-auto xl:container">
+            <GlobalNavbar />
             <Introduction />
             <Features />
+            <Footer />
         </main>
     );
 }

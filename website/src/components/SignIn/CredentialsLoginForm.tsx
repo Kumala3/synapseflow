db8583f22b2af43a18642754ac59a87ec96fde19
Kostek <ksytnik73kwer@gmail.com>
2024-05-28 20:03:07 +0200

    Update HTML code and add handleBack const

--- website/src/components/SignIn/CredentialsLoginForm.tsx
+++ website/src/components/SignIn/CredentialsLoginForm.tsx
@@ -1,15 +1,31 @@
 "use client";
 
 import { Input, Button } from "@nextui-org/react";
+import Image from "next/image";
+import BrandImage from "@public/candeel.webp";
+import LeftArrow from "../Icons/LeftArrow";
+import { useRouter } from "next/navigation";
 
 export default function CredentialsLoginForm() {
+    const router = useRouter();
+
+    const handleBack = () => {
+        // Go back to the previous page
+        router.back();
+    };
+
     return (
-        <main className="p-[100px] flex flex-col items-center">
-            <div className="honk-brand text-[100px]">
-                Candeel
+        <main className="p-20 flex flex-row items-center gap-[80px]">
+            <div className="">
+                <Image className="rounded-md w-[85%]" src={BrandImage} alt="brand-image"/>
             </div>
 
-            <div className="flex flex-col gap-4 w-[380px] p-2 rounded shadow-lg">
+            <div className="flex flex-col gap-4 p-[44px] bg-[#0d1117] w-[450px] h-[380px] rounded-md shadow-lg">
+                <Button isIconOnly className="bg-[#]" onClick={handleBack}>
+                    <LeftArrow />
+                </Button>
+
+
                 <Input
                     className=""
                     type="text"

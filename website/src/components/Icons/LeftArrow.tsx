9252f9732de9e6d3270a860ed660104fb3b9e8da
Kostek <ksytnik73kwer@gmail.com>
2024-05-28 20:01:58 +0200

    Add LeftArrow component for navigation

---
+++ website/src/components/Icons/LeftArrow.tsx
@@ -0,0 +1,47 @@
+export default function LeftArrow() {
+    return (
+        <svg
+            fill="#fff"
+            width="83px"
+            height="83px"
+            viewBox="0 0 24.00 24.00"
+            id="left-arrow"
+            data-name="Flat Line"
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon flat-line"
+            transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"
+            stroke="#fff">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <line
+                    id="primary"
+                    x1="21"
+                    y1="12"
+                    x2="3"
+                    y2="12"
+                    style={{
+                        fill: "none",
+                        stroke: "#fff",
+                        strokeLinecap: "round",
+                        strokeLinejoin: "round",
+                        strokeWidth: 2,
+                    }}></line>
+                <polyline
+                    id="primary-2"
+                    data-name="primary"
+                    points="6 9 3 12 6 15"
+                    style={{
+                        fill: "none",
+                        stroke: "#fff",
+                        strokeLinecap: "round",
+                        strokeLinejoin: "round",
+                        strokeWidth: 2,
+                    }}></polyline>
+            </g>
+        </svg>
+    );
+}

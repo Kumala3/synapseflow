d18077f2760b10134125394b1195c481e1cfe504
Kostek <ksytnik73kwer@gmail.com>
2024-06-16 20:30:07 +0200

    feat: Add form validation and error handling to SignUpLoginForm component

--- website/src/components/Auth/SingUpLoginForm.tsx
+++ website/src/components/Auth/SingUpLoginForm.tsx
@@ -13,38 +13,100 @@ interface FormState {
     confirmationPassword: string;
 }
 
+interface FormErrors {
+    email?: string;
+    password?: string;
+    confirmationPassword?: string;
+}
+
 export default function SignUpLoginForm() {
-    const pathName = usePathname();
-    const logInPath = pathName.replace(/signup$/, "signin");
     const [form, setForm] = useState<FormState>({
         email: "",
         password: "",
         confirmationPassword: "",
     });
+    const [errors, setErrors] = useState<FormErrors>({});
 
-    const handlePasswordCorrectness = () => {
-        // Check if password is meets the requirements
-    };
+    const pathName = usePathname();
+    const logInPath = pathName.replace(/signup$/, "signin");
 
+    // Handle input changes
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // Handle input changes
         const { name, value } = event.target;
         setForm(prevForm => ({ ...prevForm, [name]: value }));
+
+        // Clear errors for that field when the user starts typing
+        if (errors[name]) {
+            setErrors(prevErrors => ({ ...prevErrors, [name]: undefined }));
+        }
     };
 
+    // Clear the input field
     const handleClear = (name: string) => {
-        // Clear the input field
         setForm(prevForm => ({ ...prevForm, [name]: "" }));
     };
 
+    // Check if the email is valid
     const validateEmail = (email: string) => {
-        // Check if the email is valid
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return re.test(email);
     };
 
-    const handleRegister = () => {
-        // Register user
+    // Check if password is meets the requirements
+    const validatePasswordLength = (password: string) => {
+        if (password.length < 8) {
+            return false;
+        }
+        return true;
+    };
+
+    const validatePasswordsAccuracy = (pass1: string, pass2: string) => {
+        if (pass1 !== pass2) {
+            return false;
+        }
+        return true;
+    };
+
+    // Register user
+    const handleRegister = async () => {
+        // Validates the email
+        if (!form.email) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                email: "Please enter your email address",
+            }));
+        } else if (!validateEmail(form.email)) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                email: "Please enter a valid email address",
+            }));
+        }
+
+        // Validates the password
+        if (!form.password) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                password: "Please enter a password",
+            }));
+        } else if (!validatePasswordLength(form.password)) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                password: "Password must be at least 8 characters long",
+            }));
+        }
+
+        // Validates the confirmation password
+        if (!form.confirmationPassword) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                confirmationPassword: "Please confirm the password",
+            }));
+        } else if (!validatePasswordsAccuracy(form.password, form.confirmationPassword)) {
+            setErrors(prevErrors => ({
+                ...prevErrors,
+                confirmationPassword: "Passwords do not match",
+            }));
+        }
     };
 
     return (
@@ -65,6 +127,8 @@ export default function SignUpLoginForm() {
                     value={form.email}
                     onChange={handleChange}
                     onClear={() => handleClear("email")}
+                    errorMessage={errors.email}
+                    isRequired={true}
                 />
                 <Input
                     className=""
@@ -78,6 +142,8 @@ export default function SignUpLoginForm() {
                     value={form.password}
                     onChange={handleChange}
                     onClear={() => handleClear("password")}
+                    errorMessage={errors.password}
+                    isRequired={true}
                 />
                 <Input
                     className=""
@@ -91,6 +157,8 @@ export default function SignUpLoginForm() {
                     value={form.confirmationPassword}
                     onChange={handleChange}
                     onClear={() => handleClear("confirmationPassword")}
+                    errorMessage={errors.confirmationPassword}
+                    isRequired={true}
                 />
                 <Button size="lg" onClick={handleRegister}>
                     Register

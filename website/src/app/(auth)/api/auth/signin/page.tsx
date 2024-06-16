29aabf2baabfe8fd35827304421defd3c29c4af6
Kostek <ksytnik73kwer@gmail.com>
2024-06-16 11:45:07 +0200

    feat: Update Sign In page and add Sign Up page

--- website/src/app/(auth)/api/auth/signin/page.tsx
+++ website/src/app/(auth)/api/auth/signin/page.tsx
@@ -1,10 +1,17 @@
 import CredentialsLoginForm from "@/components/SignIn/CredentialsLoginForm";
+import BrandImage from "@public/candeel.webp";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function SignIn() {
     return (
         <main className="dark:bg-[#000000] bg-[#fff]">
-            <CredentialsLoginForm />
+            <Link href={"/"} className="">
+                <Image className="rounded-md w-[100%] h-[40%]" src={BrandImage} alt="brand-image" />
+            </Link>
+            <div className="dark:bg-[#0165f8] bg-[#fff]">
+                <CredentialsLoginForm />
+            </div>
         </main>
     );
 }
-
---
+++ website/src/app/(auth)/api/auth/signup/page.tsx
@@ -0,0 +1,7 @@
+export async function SignUp() {
+    return (
+        <main className="dark:bg-[#000000] bg-[#fff]">
+            <h1>Sign Up Page</h1>
+        </main>
+    );
+}

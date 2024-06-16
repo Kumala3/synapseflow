9211e3f77153b1ec3f7206d41f1a23d9c72d2471
Kostek <ksytnik73kwer@gmail.com>
2024-06-16 11:44:41 +0200

    refactor: Update sign in page with improved styles and components

--- website/src/components/SignIn/CredentialsLoginForm.tsx
+++ website/src/components/SignIn/CredentialsLoginForm.tsx
@@ -1,46 +1,58 @@
 "use client";
 
 import { Input, Button } from "@nextui-org/react";
-import Image from "next/image";
-import BrandImage from "@public/candeel.webp";
-import LeftArrow from "../Icons/LeftArrow";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
+import PasswordIcon from "../Icons/PasswordIcon";
+import EmailIcon from "../Icons/EmailIcon";
+import { usePathname } from "next/navigation";
 
 export default function CredentialsLoginForm() {
-    const router = useRouter();
-
-    const handleBack = () => {
-        // Go back to the previous page
-        router.back();
-    };
+    const pathName = usePathname();
+    const resetPasswordPath = pathName.replace(/signin$/, "/forgot-password/");
+    const SignUpPath = pathName.replace(/signin$/, "/signup/");
 
     return (
-        <main className="p-20 flex flex-row items-center gap-[80px]">
-            <div className="">
-                <Image className="rounded-md w-[85%]" src={BrandImage} alt="brand-image"/>
-            </div>
-
-            <div className="flex flex-col gap-4 p-[44px] bg-[#0d1117] w-[450px] h-[380px] rounded-md shadow-lg">
-                <Button isIconOnly className="bg-[#]" onClick={handleBack}>
-                    <LeftArrow />
-                </Button>
-
-
+        <main className="p-5 flex flex-column items-center">
+            <div className="flex flex-col gap-4 p-[44px] bg-[#afb3b8] w-[550px] h-[380px] rounded-md shadow-lg">
                 <Input
+                    startContent={<EmailIcon width={19} height={19} />}
                     className=""
-                    type="text"
-                    placeholder="Username"
-                    label="Username"
+                    variant="faded"
+                    type="email"
+                    placeholder="email32@gmail.com"
+                    label="Email"
                 />
                 <Input
+                    startContent={<PasswordIcon width={19} height={19} />}
                     className=""
+                    variant="faded"
                     type="password"
-                    placeholder="Password"
+                    placeholder="(min. 8 characters)"
                     label="Password"
                 />
-                <Button className="mt-1" size="md" type="submit">
-                    Sign In
+                <Button
+                    className="mt-1 font-semibold text-[18px]"
+                    size="md"
+                    type="submit">
+                    Login
                 </Button>
+                <div className="flex flex-row justify-center items-center">
+                    <Link
+                        href={resetPasswordPath}
+                        className="text-[16px] text-[#181818] font-normal hover:underline">
+                        Forgot password?
+                    </Link>
+                </div>
+                <div className="flex flex-row justify-between items-center">
+                    <div className="text-sm text-gray-600">
+                        Don&apos;t have an account?
+                    </div>
+                    <Link
+                        href={SignUpPath}
+                        className="text-blue-500 hover:underline">
+                        Sign Up
+                    </Link>
+                </div>
             </div>
         </main>
     );
